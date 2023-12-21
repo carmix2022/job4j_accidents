fa1e8b18fa402f4c8a09ae21a764d5f3cace4274
Илья <ily470@yandex.ru>
2023-12-21 18:23:51 +0700

    2. IndexController. Таблица и вид. [#2092]

--- src/main/java/ru/job4j/accidents/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -12,19 +12,14 @@ import java.util.concurrent.atomic.AtomicInteger;
 @ThreadSafe
 @Repository
 public class AccidentMem {
-    private static final AccidentMem INSTANCE = new AccidentMem();
     private final AtomicInteger nextId = new AtomicInteger(0);
     private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
 
-    private AccidentMem() {
+    public AccidentMem() {
         save(new Accident(0, "name1", "description1", "address1"));
         save(new Accident(0, "name2", "description2", "address2"));
         save(new Accident(0, "name3", "description3", "address3"));
         save(new Accident(0, "name4", "description4", "address4"));
-    }
-
-    public static AccidentMem getInstance() {
-        return INSTANCE;
     }
 
     public Accident save(Accident accident) {
